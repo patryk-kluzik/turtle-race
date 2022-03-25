ef145b4013fa04ed715374d098229d14b154f09c
patryk-kluzik <patrykkluzik99@gmail.com>
2022-03-25 11:39:11 +0000

    final turtle race

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
-screen.setup(width=500, height=400)
+width = 500
+height = 400
+screen.setup(width=width, height=height)
 
-colours = ["green", "yellow", "red", "blue", "brown"]
-turtles = [Turtle() for i in range(len(colours))]
+is_race_on = False
+winner = ''
+
+colours = ["green", "yellow", "red", "blue", "brown",  "orange"]
+turtles = [Turtle("turtle") for i in range(len(colours))]
+
 
 user_bet = screen.textinput(title="Place your bet", prompt=f"Who do you bet to win ( {', '.join(colours)} )").lower()
 
+counter = 0
+padding = 100
+space_between = (height-padding) / (len(colours)-1)
+y_axis = (height - padding)/2
 for turtle in turtles:
+    turtle.penup()
+    turtle.color(colours[counter])
+    turtle.goto(x=-235, y=y_axis)
+    y_axis -= space_between
+    counter += 1
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in turtles:
+        rand_dist = random.randint(0, 10)
+        turtle.forward(rand_dist)
+        if turtle.xcor() >= (width/2)-30:
+            is_race_on = False
+            winner = turtle.color()[0]
 
-
-#tim.goto(x=-235, y=0)
-
+if user_bet == '':
+    print("You failed to make a choice")
+elif winner == user_bet:
+    print(f"You won, You bet {user_bet} and {winner} won!")
+else:
+    print(f"You loose! You bet on {user_bet} but {winner} won.")
 screen.exitonclick()
